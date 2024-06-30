d078af08ac4362336404a83e36afd7893dbf0cfa
Irtaza <admin@IRT-MacBook-Pro.local>
2024-07-01 02:42:06 +0500

    Made the database work

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -10,7 +10,7 @@ function Signup() {
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post("", { name, email, password })
+      .post("http://localhost:3000/register", { name, email, password })
       .then((result) => console.log(result))
       .catch((error) => console.error(error));
   };
